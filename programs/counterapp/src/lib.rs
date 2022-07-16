7a7f29f522a6bad79094646b5e41d789e38d5870
paulo <pauloxti@gmail.com>
2022-07-16 14:55:52 +0200

    first commit

---
+++ programs/counterapp/src/lib.rs
@@ -0,0 +1,56 @@
+use anchor_lang::prelude::*;
+
+declare_id!("HJqSGkR5hP5d51ywhYkuwGYF4Tj3aL8A6wNS9Q5dHrM");
+
+#[program]
+pub mod counterapp {
+    use super::*;
+
+    pub fn create(ctx: Context<Create>) -> Result<()> {
+        let counter_account = &mut ctx.accounts.counter_account;
+        counter_account.count = 0;
+        Ok(())
+    }
+
+    pub fn increment(ctx: Context<Increment>) -> Result<()> {
+        let counter_account = &mut ctx.accounts.counter_account;
+        counter_account.count += 1;
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Create<'info> {
+    #[account(init, payer=user, space = 16+16)]
+    pub counter_account: Account<'info, CounterAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct CounterAccount {
+    pub count: u64,
+}
+
+/*
+
+#[account]
+pub struct CounterAccount {
+    pub count: u64,
+}
+
+*/
+
+#[account]
+pub struct CompanyAccount {
+    pub companyId: u64,   // incremental id
+    pub createdTime: u64, // date when company is created
+    pub claimDate: u64 // date when user claim
+}
+
+#[derive(Accounts)]
+pub struct Increment<'info> {
+    #[account(mut)]
+    pub counter_account: Account<'info, CounterAccount>,
+}
